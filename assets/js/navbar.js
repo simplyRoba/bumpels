49a5972cdf8e5c389c826a35037675284fff00a7
Axel <git@simplyroba.de>
2018-05-31 18:20:35 +0200

    add travis as ci tool

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-    if ($('.navbar').offset().top > 25) {
-        $('.navbar').addClass('shadow-depth-1');
-    }
-    else {
-        $('.navbar').removeClass('shadow-depth-1');
-    }
-});
