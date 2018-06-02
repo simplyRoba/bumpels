04b7254661a00f50d1ad5f79ba28fb9000d6a9f7
Axel <git@simplyroba.de>
2018-06-02 13:47:06 +0200

    working gulp configuration

--- _ts/gallery.ts
+++ _ts/gallery.ts
@@ -1,14 +1,14 @@
-const idPrefix: string = 'item-';
-const lightbox: JQuery<HTMLElement> = $('#lightbox');
+const idPrefix: string = "item-";
+const lightbox: JQuery<HTMLElement> = $("#lightbox");
 let current: number = 1;
 
-/* Bindings */ 
-$('.js-openLightbox').on('click', function() { //dont use arrow function because it would be the wrong "this"
+/* Bindings */
+$(".js-openLightbox").on("click", function() { // dont use arrow function because it would be the wrong "this"
     openLightbox(this);
 });
-$('.js-closeLightbox').on('click', closeLightbox);
-$('.js-next').on('click', next);
-$('.js-prev').on('click', prev);
+$(".js-closeLightbox").on("click", closeLightbox);
+$(".js-next").on("click", next);
+$(".js-prev").on("click", prev);
 
 function openLightbox(elem: HTMLElement): void {
     let id: string = getID(elem);
@@ -16,12 +16,12 @@ function openLightbox(elem: HTMLElement): void {
     current = removePrefixFromId(id);
     lightbox.show();
 }
-  
+
 function closeLightbox(): void {
     lightbox.hide();
 }
 
-function next():void {
+function next(): void {
     current++;
     if (current > getGallerySize()) {
         current = 1;
@@ -29,7 +29,7 @@ function next():void {
     showImageFrom(idPrefix + (current));
 }
 
-function prev():void {
+function prev(): void {
     current--;
     if (current < 0) {
         current = getGallerySize();
@@ -38,18 +38,18 @@ function prev():void {
 }
 
 function showImageFrom(id: string): void {
-    let src: string = $('#' + id).find('img').attr('src');
-    lightbox.find('img').attr('src', src);
+    let src: string = $("#" + id).find("img").attr("src");
+    lightbox.find("img").attr("src", src);
 }
 
 function removePrefixFromId(id: string): number {
-    return parseInt(id.replace(idPrefix, ''));
+    return parseInt(id.replace(idPrefix, ""));
 }
 
 function getID(elem: HTMLElement): string {
-    return $(elem).attr('id');
+    return $(elem).attr("id");
 }
 
 function getGallerySize(): number {
-    return parseInt($('.gallery').data('size'));
+    return parseInt($(".gallery").data("size"));
 }
