f2da42b3c4f25ba76619e97c81b76a202d92f912
Axel <git@simplyroba.de>
2018-06-03 14:45:49 +0200

    add imagemin and fontello

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var ts = require("gulp-typescript");
+var fontello = require("gulp-fontello");
 var sass = require("gulp-sass");
-var uglify = require('gulp-uglify');
-var pump = require('pump');
+var uglify = require("gulp-uglify");
+var imagemin = require("gulp-imagemin")
+var pump = require("pump");
 var sourcemaps = require("gulp-sourcemaps");
 var tslint = require("gulp-tslint");
-var cp = require('child_process');
+var cp = require("child_process");
 var del = require("del");
 var path = require("path");
 
@@ -22,10 +24,18 @@ var paths = {
         src: "./_sass/**/*.scss",
         main: "./_sass/main.scss"
     },
+    img: {
+        src: "./_img/**/*"
+    },
+    font: {
+        config: "./fontello-config.json"
+    },
     assets: {
+        dir: "./assets",
         js: "./assets/js",
         css: "./assets/css",
-        img: "./assets/img"
+        img: "./assets/img",
+        font: "./assets/font"
     },
     site: {
         dir: "./_site"
@@ -70,7 +80,10 @@ gulp.task("ts:build", (done) => {
         sourcemaps.write(".", {
             mapSources: (sourcePath, file) => {
                 // Correct source map path.
-                const relativeSourcePath = path.relative(path.dirname(file.path), path.join(file.base, sourcePath));
+                const relativeSourcePath = path.relative(
+                    path.dirname(file.path),
+                    path.join(file.base, sourcePath)
+                );
                 return relativeSourcePath;
             }
         }),
@@ -108,9 +121,33 @@ gulp.task("sass:build:prod", (done) => {
         gulp.dest(paths.assets.css)
     ], done);
 });
-   
+ 
 gulp.task("sass:watch", () => {
     gulp.watch(paths.sass.src, gulp.series("sass:build"));
+});
+
+gulp.task("img:build", (done) => {
+    pump([
+        gulp.src(paths.img.src),
+        imagemin(),
+        gulp.dest(paths.assets.img)
+    ], done);
+})
+
+gulp.task("img:watch", () => {
+    gulp.watch(paths.img.src, gulp.series("img:build"));
+});
+
+gulp.task("font:build", (done) => {
+    pump([
+        gulp.src(paths.font.config),
+        fontello(),
+        gulp.dest(paths.assets.dir)
+    ], done);
+});
+
+gulp.task("font:watch", () => {
+    gulp.watch(paths.font.config, gulp.series("font:build"));
 });
 
 gulp.task("jekyll:build", (done) => {
@@ -125,7 +162,7 @@ gulp.task("jekyll:serve", (done) => {
     spawn("bundle", ["exec", "jekyll", "serve" , "--drafts"], done);
 });
 
-gulp.task("optimize:prod", (done) => {
+gulp.task("optimize:js:prod", (done) => {
     pump([
         gulp.src("./assets/js/*.js"),
         uglify(),
@@ -134,7 +171,8 @@ gulp.task("optimize:prod", (done) => {
 });
 
 gulp.task("clean", (done) => {
-    return del([paths.assets.js, paths.assets.css, paths.site.dir], done);
+    return del([paths.assets.js, paths.assets.css, paths.assets.img, 
+        paths.assets.font, paths.site.dir], done);
 });
 
 gulp.task("lint", (done) => {
@@ -145,11 +183,14 @@ gulp.task("lint", (done) => {
     ], done);
 });
 
-gulp.task("watch", gulp.parallel("sass:watch", "ts:watch"));
+gulp.task("watch", gulp.parallel("sass:watch", "ts:watch", "img:watch", "font:watch"));
 
-gulp.task("build", gulp.series("clean", "ts:build", "sass:build", "jekyll:build"));
+gulp.task("build", gulp.series("clean", 
+    "ts:build", "sass:build", "img:build", "font:build", "jekyll:build"));
 
-gulp.task("build:prod", gulp.series("clean", "ts:build:prod", "sass:build:prod", "optimize:prod", "jekyll:build:prod",));
+gulp.task("build:prod", gulp.series("clean", 
+    "ts:build:prod", "sass:build:prod", "img:build", "font:build", 
+    "optimize:js:prod", "jekyll:build:prod",));
 
 //gulp.task("test", () => {});
 
