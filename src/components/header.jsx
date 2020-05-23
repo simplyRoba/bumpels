68fea9bea72e450c03a0277431efd481a1d166ee
Axel <git@simplyroba.de>
2020-05-23 09:49:18 +0200

    migrate navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,59 @@
 import { Link } from "gatsby"
 import React from "react"
+import { Navbar, Nav, Container, Button } from "react-bootstrap"
+import styles from "./header.module.scss"
 
 class Header extends React.Component {
   render() {
     return (
-      <header
-        style={{
-          background: `rebeccapurple`,
-          marginBottom: `1.45rem`,
-        }}
-      >
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
-          }}
+      <header className={styles.header}>
+        <Navbar
+          fixed="top"
+          expand="lg"
+          variant="light"
+          bg="white"
+          collapseOnSelect
+          className={styles.navShadow}
         >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            ></Link>
-          </h1>
-        </div>
+          <Container>
+            <Navbar.Brand as="div">
+              <Link to="/">
+                <img src="/static/img/logo.svg" className={styles.logo} />
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarNav" label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse className="justify-content-lg-end" id="navbarNav">
+              <Nav className={styles.navSiblingFade}>
+                <Link
+                  className={`${styles.navSibling} ${styles.navLink}`}
+                  to="/blog"
+                >
+                  Blog
+                </Link>
+                <Link
+                  className={`${styles.navSibling} ${styles.navLink}`}
+                  to="/gallery"
+                >
+                  Gallery
+                </Link>
+                <Link
+                  className={`${styles.navSibling} ${styles.navLink}`}
+                  to="/merchandise"
+                >
+                  Merchandise
+                </Link>
+                <Link
+                  className={`${styles.navSibling} ${styles.navLink}`}
+                  to="/about"
+                >
+                  About
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     )
   }
